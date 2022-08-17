dec868b7641fff914c1eedad736751e595e0ebda
niludarekar1 <nilu.darekar1@gmail.com>
2022-08-17 13:57:43 +0530

    Added:MYSQL Emppayroll Application

--- src/main/java/com/bridgelabz/employeepayrollapplicationdemo/service/IEmployeeService.java
+++ src/main/java/com/bridgelabz/employeepayrollapplicationdemo/service/IEmployeeService.java
@@ -1,8 +1,13 @@
 package com.bridgelabz.employeepayrollapplicationdemo.service;
 
 import com.bridgelabz.employeepayrollapplicationdemo.entity.EmployeeEntity;
+import org.springframework.web.servlet.ModelAndView;
 
 
 public interface IEmployeeService {
     EmployeeEntity addEmployee(EmployeeEntity employeeEntity);
+
+    void addEmployee2(EmployeeEntity employeeEntity);
+
+    ModelAndView getAllEmp();
 }
