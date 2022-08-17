c24bb870a9462b459f453b32dacb73ec4fc43912
niludarekar1 <nilu.darekar1@gmail.com>
2022-08-17 13:31:09 +0530

    Added:Setting up the new Payroll Application

--- src/main/java/com/bridgelabz/employeepayrollapplicationdemo/controller/EmployeeController.java
+++ src/main/java/com/bridgelabz/employeepayrollapplicationdemo/controller/EmployeeController.java
@@ -1,19 +1,36 @@
 package com.bridgelabz.employeepayrollapplicationdemo.controller;
 
 
+import com.bridgelabz.employeepayrollapplicationdemo.entity.EmployeeEntity;
+import com.bridgelabz.employeepayrollapplicationdemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class EmployeeController {
 
-    @GetMapping("/emphome")
+    //Autowired Employee Service
+    @Autowired
+    EmployeeService employeeService;
+
+    //URL Request Mapping for Employee Home View Page i.e. index.html
+    //URL - http://localhost:8080/emphome
+    @RequestMapping("/emphome")
     public String openHome1() {
         return "index";
     }
 
-    @GetMapping("/emppayroll")
+    //URL Request Mapping for Employee Payroll Page i.e. emppayroll.html
+    //URL - http://localhost:8080/emppayroll
+    @RequestMapping(value="/emppayroll")
     public String openHome2() {
         return "emppayroll";
+    }
+
+    //POST Mapping for Employee Payroll Data to be stored in DB by Employee Service
+    @RequestMapping(value="/saveemp", method= RequestMethod.POST)
+    public @ResponseBody EmployeeEntity test(EmployeeEntity employeeEntity){
+        return employeeService.addEmployee(employeeEntity);
     }
 
 }
